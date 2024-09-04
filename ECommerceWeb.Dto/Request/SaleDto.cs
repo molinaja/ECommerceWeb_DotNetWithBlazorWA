cb106a41d06b7c0141fd8118379e7735b23c8901
JuanMolinaVargas <jmolinav@crnova.com>
2024-09-04 00:00:39 -0600

    cart and checkout dev

---
+++ ECommerceWeb.Dto/Request/SaleDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceWeb.Dto.Request
+{
+    public record SaleDto(int ClientID, float Total, ICollection<SaleDetailDto> SaleDetail );
+
+    public record SaleDetailDto(int productID, int queantity, float Price, float Total);
+
+}
